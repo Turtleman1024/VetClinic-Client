6f506f9d99aa8f8b37efedaf0e5aae410370504b
Turtleman1024 <kmurry6262@gmail.com>
2023-07-14 22:42:09 -0700

    Adding redux toolkit.

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
+// https://redux.js.org/usage/migrating-to-modern-redux
 const initialState = {};
 
 const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  applyMiddleware(sagaMiddleware)
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware,
+  preloadedState: initialState,
+});
 
 sagaMiddleware.run(rootSaga);
 
