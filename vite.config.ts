7850750b0dc7c1325b49953019318260d859599f
Turtleman1024 <kmurry6262@gmail.com>
2023-07-15 23:11:04 -0700

    CRS To Vite

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react'
+import svgrPlugin from 'vite-plugin-svgr'
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+        return {
+                server: {
+                        open: true,
+                        host: 'localhost',
+                        port: 3000
+                },
+                // This changes the out put dir from dist to build
+                // comment this out if that isn't relevant for your project
+                build: {
+                        outDir: 'build',
+                },
+                plugins: [
+                        reactRefresh(),
+                        svgrPlugin(),
+                ],
+        };
+})
